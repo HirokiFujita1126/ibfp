54d34d488dd3d24ec5d0e2ebd5e6f078a850a657
Hiroki Fujita <40275616+HirokiFujita1126@users.noreply.github.com>
2024-10-29 12:41:00 +0100

    zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,3 +3,10 @@
   Material <- read.table(data_file_path, header = FALSE, col.names = paste0("a",seq_len(18)), fill = TRUE)
   assign("Material", Material, envir = .GlobalEnv)
 }
+
+.onLoad <- function(libname, pkgname) {
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("Package 'dplyr' needed but not installed.")
+  }
+  library(dplyr, character.only = TRUE)
+}
